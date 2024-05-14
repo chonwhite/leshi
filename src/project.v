f2e38aa371183cb6d9014041cfc5e55c0bf4188c
Chon <arthurbrown@163.com>
2024-05-14 23:12:28 +0800

    Update project.v

--- src/project.v
+++ src/project.v
@@ -20,8 +20,12 @@ module tt_um_example (
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
+  wire reset;
 
-    reg [7:0] count;
+  // Logic to derive reset signal
+  assign reset = ~reset_n;
+
+  reg [7:0] count;
 
   always @(posedge clk or posedge reset) begin
     if (reset)
